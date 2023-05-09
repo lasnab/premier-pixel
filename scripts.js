d05719be40c610774b5c9f16fcc578fe15fdaf63
lasnab <hrithikbansal70@gmail.com>
2023-05-08 23:20:28 -0400

    init

---
+++ scripts.js
@@ -0,0 +1,39 @@
+function handleNavOpen() {
+  var x = document.getElementById('overlayNav');
+  if (x.className === 'overlay-nav') {
+    x.className += ' responsive';
+  }
+}
+
+function handleNavClose() {
+  var x = document.getElementById('overlayNav');
+  if (x.className === 'overlay-nav responsive') {
+    x.className = 'overlay-nav';
+  }
+}
+
+var NUMBER_OF_STARS = 115;
+
+var addPulse = function (element) {
+  var pulseTime = Math.random() * 4000;
+  setTimeout(function () {
+    element.className += ' pulse';
+  }, pulseTime);
+};
+
+for (var jess = 0; jess < NUMBER_OF_STARS; jess++) {
+  var aStar = document.createElement('div');
+  aStar.className = 'star';
+
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+
+  var x = Math.random() * windowWidth;
+  aStar.style.left = x + 'px';
+  document.body.appendChild(aStar);
+
+  var y = Math.random() * windowHeight;
+  aStar.style.top = y + 'px';
+
+  addPulse(aStar);
+}
